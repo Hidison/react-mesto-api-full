62844468c4d6a14f81192979ab676cdc905253de
Aleksandr Aleksandrov <sash.aleksandrov2011@yandex.ru>
2020-11-02 18:03:56 +0300

    попытка 2

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 app.use('/cards', cardRouter);
 app.use('/users', userRouter);
 app.use((req, res) => {
-  res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+
+});
--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,6 @@
 const cardRouter = require('express').Router();
-const path = require('path');
-const fsPromises = require('fs').promises;
 
-const getCards = (req, res) => {
-fsPromises.readFile(path.join(__dirname, '..', 'data', 'cards.json'))
-    .then((data) => {
-      res.status(200).send(JSON.parse(data))
-    })
-    .catch(err => {
-      res.status(500).send({message: `Ошибка: ${err}`})
-    });
-  }
+const { getCards } = require('../utils/read-file');
 
 cardRouter.get('/', getCards);
 

dbfb83ef0e5a18c0ee02146f6d5f6afd649d18cf
Aleksandr Aleksandrov <sash.aleksandrov2011@yandex.ru>
2020-12-09 21:45:02 +0300

    попытка 1

--- backend/app.js
+++ backend/app.js
@@ -27,18 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
+app.use(cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {

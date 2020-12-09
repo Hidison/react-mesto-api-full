55fc430af90aea30fc322988d1f5b93904d33acd
Aleksandr Aleksandrov <sash.aleksandrov2011@yandex.ru>
2020-12-09 22:01:25 +0300

    попытка 1

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,12 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(cors());
-app.options('*', cors());
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+app.use(express.json(), cors(corsOptions));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
